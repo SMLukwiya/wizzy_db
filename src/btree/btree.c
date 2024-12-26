176f92d6cfb2dd16d2e794969d7119a8185e6995
SMLukwiya <sundaymorganl@gmail.com>
2024-12-26 14:50:19 +0300

    split node

--- src/btree/btree.c
+++ src/btree/btree.c
@@ -1,9 +1,10 @@
 #include "btree.h"
+#include "pager.h"
 
 #include <stdlib.h>
 
 /* Create a new B+ tree node */
-static BNODE *createNode(bool isLeaf) {
+static BNODE *create_node(bool isLeaf) {
     int i;
     BNODE *newNode = (BNODE *)malloc(sizeof(BNODE));
     if (!newNode) {
@@ -32,6 +33,70 @@ static BNODE *createNode(bool isLeaf) {
     }
 
     return newNode;
+}
+
+/* Split child node that is full */
+void split_child(BPTREE *tree, BNODE *parent, int index) {
+    BNODE *childNode;
+    int i;
+    /* retrieve child node to split */
+    uint64 childNodeOffset = parent->bInternal.child_offsets[index];
+    load_node(childNodeOffset, childNode);
+
+    BNODE *newNode = create_node(childNode->isLeaf);
+    newNode->offset = tree->next_page_offset;
+
+    int middleIndex = M / 2;
+    /* Leaf nodes retain all keys after split
+     * Internal nodes remove middle key from child and places in parent
+     * Therefore one less key is retained
+     */
+    newNode->numOfKeys = newNode->isLeaf ? middleIndex : middleIndex - 1;
+    /* move next_page_offset to next free space by M */
+    tree->next_page_offset += 4096;
+
+    /* Copy keys and child offsets to new node */
+    for (i = 0; i < newNode->numOfKeys; i++) {
+        if (newNode->isLeaf) {
+            newNode->bLeaf.keys[i] = childNode->bLeaf.keys[i + middleIndex];
+            newNode->bLeaf.data_offsets[i] = childNode->bLeaf.data_offsets[i + middleIndex];
+        } else {
+            newNode->bInternal.keys[i] = childNode->bInternal.keys[i + M / 2];
+            newNode->bInternal.child_offsets[i] = childNode->bInternal.child_offsets[i + M / 2];
+        }
+    }
+    childNode->numOfKeys = childNode->isLeaf ? middleIndex : middleIndex - 1;
+
+    /* Update next node pointer for leaf node
+     * new node points to whatever child node was pointing to
+     * child node points to new node
+     */
+    if (newNode->isLeaf) {
+        newNode->bLeaf.next_offset = childNode->bLeaf.next_offset;
+        childNode->bLeaf.next_offset = newNode->offset;
+    }
+
+    /* shift parent node keys and child offsets to make room for new node */
+    for (i = parent->numOfKeys; i > index; i--) {
+        parent->bInternal.keys[i] = parent->bInternal.keys[i - 1];
+        parent->bInternal.child_offsets[i + 1] = parent->bInternal.child_offsets[i];
+    }
+    /* Promote middle child key to parent key array */
+    if (childNode->isLeaf) {
+        parent->bInternal.keys[index] = childNode->bLeaf.keys[middleIndex];
+    } else {
+        parent->bInternal.keys[index] = childNode->bInternal.keys[middleIndex - 1];
+    }
+    parent->numOfKeys++;
+    parent->bInternal.child_offsets[index + 1] = newNode->offset;
+
+    /* Persist update to disk */
+    save_node(childNode->offset, childNode);
+    save_node(newNode->offset, newNode);
+    save_node(parent->offset, parent);
+
+    free(childNode);
+    free(newNode);
 }
 
 /* Create B+ tree */
